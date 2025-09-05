295079202efda8e5cb4f1754a92f0758117f5bdb
Christopher R. Palmer <crpalmer@gmail.com>
2025-09-05 08:13:41 -0400

    disable selinux for the livecd

--- mkksiso.sh
+++ mkksiso.sh
@@ -8,6 +8,7 @@ fi
 sudo mkksiso --skip-mkefiboot \
 	-a boot \
 	-a LiveOS \
+	--cmdline 'selinux=0' \
 	--replace 'initrd ' "devicetree /boot/dtbs/$1/qcom/x1e80100-lenovo-yoga-slim7x.dtb 
 initrd " \
 	--rm-args "quiet rhgb" \
